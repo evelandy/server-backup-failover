6ad8df1bc3a6fe02bbfe728f11fbe63e7ce736b4
Will G <willg@Wills-MacBook-Pro.local>
2023-08-08 17:23:34 -0500

    Added functional code

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log('test');
+const https = require('https');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const siteURL = process.env.SITE;
+
+const checkSite = (siteURL) => {
+  const mainSite = siteURL;
+  const result = https.request(mainSite);
+  result.end();
+
+  let promise = new Promise((resolve, reject) => {
+    let connected = false;
+    result.on('response', (res) => {
+      connected = res.statusCode < 500;
+      resolve(connected);
+      // console.log(connected);
+    })
+    result.on('error', (err) => {
+      resolve(false);
+      // console.log(false);
+    })
+  })
+  return promise;
+  // console.log(result.statusCode);
+}
+
+// console.log(checkSite(siteURL));
+checkSite(siteURL).then((isAvailable) => {
+  if(isAvailable){
+    console.log('good')
+  } else {
+    console.log('no good')
+  }
+});
